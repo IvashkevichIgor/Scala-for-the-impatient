c42d1ceea147247221569febd31c41de58038692
Igor Ivashkevich <ivashkevich.java@gmail.com>
2023-07-19 11:36:16 +0300

    add exercise 1 from chapter 3

---
+++ src/main/scala/chapter_3/Exercise1.scala
@@ -0,0 +1,20 @@
+package ru.ivashkevich
+package chapter_3
+
+import scala.util.Random
+
+object Exercise1 {
+  /** Write a code snippet that sets a to an array of n random integers between 0
+   *  (inclusive) and n (exclusive). */
+  def func(n: Int): Array[Int] = {
+    val array = new Array[Int](n)
+    for(_ <- array) yield Random.between(0, n)
+  }
+
+  def test(n: Int) : Unit = {
+    println("Exercise 1:")
+    println(s"Getting array of $n random integers between 0 and $n:")
+    println(s"${Exercise1.func(n).mkString("Array(", ", ", ")")}")
+    println("-------------------------------------------------")
+  }
+}
---
+++ src/main/scala/chapter_3/Main.scala
@@ -0,0 +1,10 @@
+package ru.ivashkevich
+package chapter_3
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    Exercise1.test(10)
+
+  }
+
+}

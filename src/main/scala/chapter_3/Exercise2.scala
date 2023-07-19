3f6f3816145e162c934c2e67ad631ec639476ad7
Igor Ivashkevich <ivashkevich.java@gmail.com>
2023-07-19 12:15:56 +0300

    add exercise 2 from chapter 3

---
+++ src/main/scala/chapter_3/Exercise2.scala
@@ -0,0 +1,22 @@
+package ru.ivashkevich
+package chapter_3
+
+object Exercise2 {
+  /** Write a loop that swaps adjacent elements of an array of integers. For example,
+   *  Array(1, 2, 3, 4, 5) becomes Array(2, 1, 4, 3, 5). */
+  def func(a: Array[Int]): Array[Int] = {
+    for(i <- 1 until a.length by 2) {
+      val b = a(i)
+      a(i) = a(i-1)
+      a(i-1) = b
+    }
+    a
+  }
+
+  def test(a: Array[Int]): Unit = {
+    println("Exercise 2:")
+    println(s"Initial array: ${a.mkString("Array(", ", ", ")")}")
+    println(s"Array after using function: ${Exercise2.func(a).mkString("Array(", ", ", ")")}")
+    println("-------------------------------------------------")
+  }
+}
--- src/main/scala/chapter_3/Main.scala
+++ src/main/scala/chapter_3/Main.scala
@@ -4,6 +4,7 @@ package chapter_3
 object Main {
   def main(args: Array[String]): Unit = {
     Exercise1.test(10)
+    Exercise2.test(Array(1, 2, 3, 4, 5, 8, 9))
 
   }
 

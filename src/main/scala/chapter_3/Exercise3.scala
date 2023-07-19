0d9f3198c3a4fa75ca0f8cecbffeee9ee673c603
Igor Ivashkevich <ivashkevich.java@gmail.com>
2023-07-19 13:58:01 +0300

    add exercise 3 from chapter 3

---
+++ src/main/scala/chapter_3/Exercise3.scala
@@ -0,0 +1,21 @@
+package ru.ivashkevich
+package chapter_3
+
+object Exercise3 {
+  /** Repeat the preceding assignment, but produce a new array with the swapped
+   *  values. Use for/yield. */
+  def func(a: Array[Int]): Array[Int] = {
+    (for (i <- a.indices) yield {
+      if (i % 2 == 0) {if ((i + 1) == a.length) a(i)
+                       else a(i + 1)}
+      else a(i - 1)
+    }).toArray
+  }
+
+  def test(a: Array[Int]): Unit = {
+    println("Exercise 3:")
+    println(s"Initial array: ${a.mkString("Array(", ", ", ")")}")
+    println(s"Array after using function with for/yield: ${Exercise3.func(a).mkString("Array(", ", ", ")")}")
+    println("-------------------------------------------------")
+  }
+}
--- src/main/scala/chapter_3/Main.scala
+++ src/main/scala/chapter_3/Main.scala
@@ -5,6 +5,7 @@ object Main {
   def main(args: Array[String]): Unit = {
     Exercise1.test(10)
     Exercise2.test(Array(1, 2, 3, 4, 5, 8, 9))
+    Exercise3.test(Array(1, 2, 3, 4, 5, 8, 9))
 
   }
 

5656d2054dc67c651352001cce8960f05c65e218
Igor Ivashkevich <ivashkevich.java@gmail.com>
2023-07-24 09:51:29 +0300

    add exercise 5 from chapter 5

--- src/main/scala/chapter_05/Main.scala
+++ src/main/scala/chapter_05/Main.scala
@@ -8,5 +8,6 @@ object Main {
     Ex03.test(new Ex03.Time(11, 23), new Ex03.Time(11, 48))
     Ex03.test(new Ex03.Time(4, 20), new Ex03.Time(0, 12))
     Ex04.test(new Ex04.Time(5, 32), new Ex04.Time(11, 48))
+    Ex05.test(new Ex05.Student("Igor", 123))
   }
 }

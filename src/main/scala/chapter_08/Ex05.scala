d420c1fbb42799a9741f8a9dc896bfe39fd7be7e
Igor Ivashkevich <ivashkevich.java@gmail.com>
2023-07-25 16:23:15 +0300

    add exercise 5 from chapter 8

---
+++ src/main/scala/chapter_08/Ex05.scala
@@ -0,0 +1,23 @@
+package ru.ivashkevich
+package chapter_08
+
+object Ex05 extends App {
+  /** Design a class Point whose x and y coordinate values can be provided in a
+   *  constructor. Provide a subclass LabeledPoint whose constructor takes a label
+   *  value and x and y coordinates, such as
+   *  new LabeledPoint("Black Thursday", 1929, 230.07) */
+
+  test()
+
+  class Point(val x: Double, val y: Double){
+
+  }
+
+  class LabeledPoint(val label: String, x: Double, y: Double) extends Point(x, y){
+
+  }
+
+  def test(): Unit = {
+    println(new LabeledPoint("Black Thursday", 1929, 230.07))
+  }
+}

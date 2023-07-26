23eaa82845029ae141350bc99ca0deabd9c64ea0
Igor Ivashkevich <ivashkevich.java@gmail.com>
2023-07-26 16:02:36 +0300

    add exercise 5 from chapter 9

--- src/main/scala/chapter_09/Ex05.scala
+++ src/main/scala/chapter_09/Ex05.scala
@@ -6,10 +6,10 @@ import java.io.{File, PrintWriter}
 object Ex05 extends App {
   /** Write a Scala program that writes the powers of 2 and their reciprocals to a
    *  file, with the exponent ranging from 0 to 20. Line up the columns:
-   *  1   1
-   *  2   0.5
-   *  4   0.25
-   *  ... ... */
+   *    1   1
+   *    2   0.5
+   *    4   0.25
+   *  ...   ... */
 
   func(new java.io.File("C:\\Java\\projects\\scala_for_the_impatient\\src\\files\\chapter_09\\ex05.txt"))
 
@@ -19,7 +19,7 @@ object Ex05 extends App {
     }.toArray
     val maxLength = arr.last._1.toString.length
     val out = new PrintWriter(file)
-    for ((a, b) <- arr) out.println(s"${a.toInt} ${" " * (maxLength - a.toString.length)} $b")
+    for ((a, b) <- arr) out.println(s"${" " * (maxLength - a.toString.length)} ${a.toInt}${"\t" * 2}$b")
     out.close()
   }
 }

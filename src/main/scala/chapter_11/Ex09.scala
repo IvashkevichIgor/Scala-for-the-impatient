13e9e2650527285023f305f3b48e4095eb6d0e36
Igor Ivashkevich <ivashkevich.java@gmail.com>
2023-08-21 09:28:07 +0300

    fix exercise 9 from chapter 11 Change return type of unapply method to Option

--- src/main/scala/chapter_11/Ex09.scala
+++ src/main/scala/chapter_11/Ex09.scala
@@ -9,18 +9,17 @@ object Ex09 extends App{
    *  file /home/cay/readme.txt has directory path /home/cay and file name readme.txt. */
 
   object PathComponents {
-    def unapply(path: Path): (Path, String) = {
+    def unapply(path: Path): Option[(Path, String)] = {
       val sPath = path.toString
       val index = sPath.lastIndexOf("\\")
-      val directory = Paths.get(sPath.take(index))
-      val fileName = sPath.drop(index + 1)
-      (directory, fileName)
+      if (index == -1) None
+      else Some(Paths.get(sPath.take(index)), sPath.drop(index + 1))
     }
   }
 
   println("Exercise 9:")
-  val tuple = PathComponents.unapply(Paths.get("/home/cay/readme.txt"))
-  println(s"Full path: /home/cay/readme.txt")
+  val tuple = PathComponents.unapply(Paths.get("/home/cay/readme.txt")).get
+  println(s"Full path: \\home\\cay\\readme.txt")
   println(s"Directory: ${tuple._1}")
   println(s"File name: ${tuple._2}")
   println("-------------------------------------------------")
